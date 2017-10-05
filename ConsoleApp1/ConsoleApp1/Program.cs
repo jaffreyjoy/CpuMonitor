8b8dbd2f741a7f5602c83f98c5c2b30c9c5c4e9c
Arushi <rushanne284@gmail.com>
2017-10-05 21:49:58 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunSchedule();
+        }
+
+        public static void RunSchedule()
+        {
+            string path = Path.GetFullPath("C:\\MyTest") + "\\"
+                            + DateTime.Now.ToString("MM_dd_yyyy_HH_mm") + "_Log.txt";
+
+            try
+            {
+                if(!File.Exists(path))
+                {
+                    //File.Create(path);
+                    string errorLogPath = @"C:\MyTest.txt";
+
+                    string createText = "Hello" + Environment.NewLine;
+                    string appendText = "This is insane" + Environment.NewLine;
+
+                    while (true)
+                    {
+
+                        File.WriteAllText(path, createText);
+                        File.AppendAllText(errorLogPath, appendText);
+
+                    }
+                }
+            }
+
+            catch(Exception ex)
+            {
+                string errorLogPath = @"C:\MyTest.txt";
+                string appendText = "This is insane" + Environment.NewLine;
+                File.AppendAllText(errorLogPath, appendText);
+            }
+        }
+    }
+}
