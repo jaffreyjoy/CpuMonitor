642df41b4253c3b13d18a20a8c434b077e3afa36
Jaffrey Joy <jaffreyjoy@gmail.com>
2017-10-05 23:44:07 +0530

    showing CPU usage in terminal window

---
+++ CpuMonitor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace CpuMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PerformanceCounter cpuCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+            while (true)
+            { 
+                Console.Write("CPU Usage : {0}% \n", cpuCount.NextValue());
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
