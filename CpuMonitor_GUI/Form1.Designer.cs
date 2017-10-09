4d48c79c6adef2eebd0cc9c93a5ac481e54f0767
Jaffrey Joy <jaffreyjoy@gmail.com>
2017-10-09 09:08:17 +0530

    icon changed

--- CpuMonitor_GUI/Form1.Designer.cs
+++ CpuMonitor_GUI/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.cpu_text = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -138,6 +139,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cpu_text);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cpu Monitor";
